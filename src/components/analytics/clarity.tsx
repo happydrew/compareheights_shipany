4ae48b211ce50b28bfa740cce8f249c38596a798
zhugechina <zhugetd@126.com>
2025-10-20 19:42:33 +0800

    update

--- src/components/analytics/clarity.tsx
+++ src/components/analytics/clarity.tsx
@@ -18,11 +18,11 @@ export default function Clarity() {
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "${projectId}");
+           (function(c,l,a,r,i,t,y){
+             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+           })(window, document, "clarity", "script", "${projectId}");
         `,
       }}
     />
